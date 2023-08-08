24abd787ad848e397bfc709b3e32fb90d04d7f19
Pavel Demin <pavlikdemin@yandex.ru>
2023-08-09 01:28:49 +0300

    Сделал все на 1280. Отпикселил 404 Main

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -1,23 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Portfolio.css';
 import link from '../../../images/link_pic.svg';
 
 const Portfolio = (props) => {
   return (
-   <section className="portfolio">
+   <section className="portfolio__container">
      <h3 className="portfolio__tiitle">Портфолио</h3>
      <ul className="portfolio__list">
       <li className="portfolio__list-item">
-        <p className="portfolio__item-name">Статичный сайт</p>
-        <a href="https://energetikk.github.io/how-to-learn/" target='_blank' className="portfolio__link"><img src={link} alt="символ стрелки" className="portfolio__link-pic"/></a>
+        <Link to="https://energetikk.github.io/how-to-learn/" target='_blank' className="portfolio__link"><p className="portfolio__item-name">Статичный сайт</p><img src={link} alt="символ стрелки" className="portfolio__link-pic"/></Link>
       </li>
       <li className="portfolio__list-item">
-        <p className="portfolio__item-name">Адаптивный сайт</p>
-        <a href="https://energetikk.github.io/russian-travel/" target='_blank' className="portfolio__link"><img src={link} alt="символ стрелки" className="portfolio__link-pic"/></a>
+        <Link to="https://energetikk.github.io/russian-travel/" target='_blank' className="portfolio__link"><p className="portfolio__item-name">Адаптивный сайт</p><img src={link} alt="символ стрелки" className="portfolio__link-pic"/></Link>
       </li>
       <li className="portfolio__list-item">
-        <p className="portfolio__item-name">Одностраничное приложение</p>
-        <a href="https://energetikk.github.io/mesto/" target='_blank' className="portfolio__link"><img src={link} alt="символ стрелки" className="portfolio__link-pic"/></a>
+        <Link to="https://energetikk.github.io/mesto/" target='_blank' className="portfolio__link"><p className="portfolio__item-name">Одностраничное приложение</p><img src={link} alt="символ стрелки" className="portfolio__link-pic"/></Link>
       </li>
      </ul>
    </section>
